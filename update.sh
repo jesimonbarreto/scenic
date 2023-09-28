9e9b2aa643702770b92e6c901c926887a269b4dc
Jesimon Barreto <jesimonbarreto@github.com>
2023-09-28 17:16:14 -0300

    Updating

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "Updating"
-git push https://jesimonbarreto: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@github.com/jesimonbarreto/scenic.git
+git push https://jesimonbarreto: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@github.com/jesimonbarreto/scenic.git
 
