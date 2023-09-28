9a39c54d04fee8c9b3c492f6b1f3730289ffbaba
Jesimon Barreto <jesimonbarreto@github.com>
2023-09-28 12:51:27 -0300

    Adding imagenette

--- scenic/dataset_lib/imagenette_dataset.py
+++ scenic/dataset_lib/imagenette_dataset.py
@@ -255,7 +255,7 @@ def imagenet_load_split(batch_size,
     label = tf.one_hot(label, NUM_CLASSES) if onehot_labels else label
     return {'inputs': image, 'label': label}
 
-  dataset_builder = tfds.builder('imagenet2012:5.*.*')
+  dataset_builder = tfds.builder('imagenette')
   # Download dataset:
   dataset_builder.download_and_prepare()
   ds = dataset_builder.as_dataset(
