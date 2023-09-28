738210104671375b974bcebcc09c8c0edbbc43a4
Jesimon Barreto <jesimonbarreto@github.com>
2023-09-28 15:24:16 -0300

    test

--- scenic/dataset_lib/datasets.py
+++ scenic/dataset_lib/datasets.py
@@ -37,7 +37,7 @@ _IMPORT_TABLE = {
     'imagenet': 'scenic.dataset_lib.imagenet_dataset',
     'tinyImagenet': 'scenic.dataset_lib.tinyImagenet_dataset',
     'fashion_mnist': 'scenic.dataset_lib.fashion_mnist_dataset',
-    #'mnist': 'scenic.dataset_lib.mnist_dataset',
+    'mnist': 'scenic.dataset_lib.mnist_dataset',
     'bair': 'scenic.dataset_lib.bair_dataset',
     'oxford_pets': 'scenic.dataset_lib.oxford_pets_dataset',
     'svhn': 'scenic.dataset_lib.svhn_dataset',
@@ -101,6 +101,8 @@ class DatasetRegistry(object):
     Raises:
       KeyError: If the dataset is not found.
     """
+    raise KeyError(f'Unknown dataset ({name}). Did you import the dataset '
+                       f'module explicitly?')
     if name not in cls._REGISTRY:
       if name in _IMPORT_TABLE:
         module = _IMPORT_TABLE[name]
