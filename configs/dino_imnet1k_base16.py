ab2717d3e431667deb5b9d82942408b620a3ddfb
Jesimon Barreto <jesimonbarreto@github.com>
2023-11-22 12:19:31 -0300

    Updating

--- configs/dino_imnet1k_base16.py
+++ configs/dino_imnet1k_base16.py
@@ -34,6 +34,19 @@ def get_config():
       '|random_grayscale(0.2, data_key="x1")' +
       '|random_blur(1.0, data_key="x1")' +
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="x1")' +
+      'decode' +
+      '|copy("image", "x2")' +
+      '|init_patch_matching_tracker(14, "target_mask")' +
+      '|init_box_tracker("target_box")' +
+      f'|cropflip_generatemask({reference_resolution}, 32, flip=False, inkey=("x2", "target_mask", "target_box"), outkey=("x2", "target_mask", "target_box"))' +
+      '|value_range(0, 1, data_key="x2")' +
+      '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x2")' +
+      '|random_grayscale(0.2, data_key="x2")' +
+      '|random_blur(1.0, data_key="x2")' +
+      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="x2")' +
+      
+      """
+      
       ''.join([f'|copy("image", "query{i}")' for i in range(n_queries)]) +
       '|inception_crop_with_mask((224, 224), 32, 100, (14, 14), inkey=("query0", "target_mask", "target_box"), outkey=("query0", "query0_mask", "query0_box"))' +
       ''.join([f'|inception_crop_with_mask((96, 96), 5, 32, (6, 6), inkey=("query{i}", "target_mask", "target_box"), outkey=("query{i}", "query{i}_mask", "query{i}_box"))' for i in range(1, n_queries)]) +
@@ -45,6 +58,7 @@ def get_config():
       '|random_blur(0.1, data_key="query0")|random_solarize(0.2, data_key="query0")' +
       ''.join([f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="query{i}")' for i in range(n_queries)]) +
       '|keep("x1"' + ''.join([f', "query{i}", "query{i}_box", "query{i}_mask"' for i in range(n_queries)]) + ')')
+      """
   
   # For IMAGENET-1K
   #config.dataset_configs.dataset = 'imagenet2012'
