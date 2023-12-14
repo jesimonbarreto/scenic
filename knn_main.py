ba7be4d3696814f0ed8fdf316da9268afa407528
Jesimon Barreto <jesimonbarreto@github.com>
2023-12-14 20:47:43 -0300

    Updating

--- knn_main.py
+++ knn_main.py
@@ -36,7 +36,7 @@ def get_datasets(batch=3):
     ds_builder = tfds.builder('imagenette')
     ds_builder.download_and_prepare()
     train_ds = tfds.as_numpy(ds_builder.as_dataset(split='train', batch_size=batch))
-    test_ds = tfds.as_numpy(ds_builder.as_dataset(split='test', batch_size=batch))
+    test_ds = tfds.as_numpy(ds_builder.as_dataset(split='validation', batch_size=batch))
     train_ds['image'] = jnp.float32(train_ds['image']) / 255.
     test_ds['image'] = jnp.float32(test_ds['image']) / 255.
     return train_ds, test_ds
