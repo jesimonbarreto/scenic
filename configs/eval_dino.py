99ec2974daf1facdf3187f77dde788eb39bd4c6d
Jesimon Barreto <jesimonbarreto@github.com>
2023-12-15 08:25:52 -0300

    Updating

--- configs/eval_dino.py
+++ configs/eval_dino.py
@@ -73,7 +73,7 @@ def get_config():
   # Training.
   config.max_grad_norm = 1
   config.num_training_epochs = 100
-  config.batch_size = 1024
+  config.batch_size = 8
   config.steps_per_epoch = _IMAGENET_TRAIN_SIZE // config.batch_size
   config.rng_seed = 42
   total_steps = config.num_training_epochs * config.steps_per_epoch
