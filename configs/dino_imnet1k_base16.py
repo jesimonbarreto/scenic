1eec2976e56aab437f8741c255f31224a3b97015
Jesimon Barreto <jesimonbarreto@github.com>
2023-12-19 10:40:25 -0300

    Updating

--- configs/dino_imnet1k_base16.py
+++ configs/dino_imnet1k_base16.py
@@ -30,7 +30,7 @@ def get_config():
       '|copy("image", "x2")' +
       #'|copy_resize_file("image", "x1")' +
       #'|copy_resize_file("image", "x2")' +
-      '|init_patch_matching_tracker(14, "target_mask")' +
+      ''' '|init_patch_matching_tracker(14, "target_mask")' +
       '|init_box_tracker("target_box")' +
       f'|cropflip_generatemask({reference_resolution}, 32, flip=False, inkey=("reference", "target_mask", "target_box"), outkey=("reference", "target_mask", "target_box"))' +
       '|value_range(0, 1, data_key="reference")' +
@@ -38,13 +38,13 @@ def get_config():
       '|random_grayscale(0.2, data_key="reference")' +
       '|random_blur(1.0, data_key="reference")' +
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="reference")' +
-
+      '|copy("image", "x1")' +
       '|value_range(0, 1, data_key="x1")' +
       '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x1")' +
       '|random_grayscale(0.2, data_key="x1")' +
       '|random_blur(1.0, data_key="x1")' +
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="x1")' +
-      
+      '|copy("image", "x2")' +
       '|value_range(0, 1, data_key="x2")' +
       '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x2")' +
       '|random_grayscale(0.2, data_key="x2")' +
@@ -62,7 +62,7 @@ def get_config():
       '|random_blur(0.1, data_key="query0")|random_solarize(0.2, data_key="query0")' +
       ''.join([f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="query{i}")' for i in range(n_queries)]) +
       '|keep("reference"' + ''.join([f', "query{i}", "query{i}_box", "query{i}_mask"' for i in range(n_queries)]) + ')')
-      
+      '''
   
   # For IMAGENET-1K
   #config.dataset_configs.dataset = 'imagenet2012'
