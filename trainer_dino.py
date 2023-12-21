90ed828db1292d0c8f9024fefefcbdd24758c5cb
Jesimon Barreto <jesimonbarreto@github.com>
2023-12-21 17:35:41 -0300

    Updating

--- trainer_dino.py
+++ trainer_dino.py
@@ -237,7 +237,7 @@ def train(
           config=config),
       axis_name='batch',
       # We can donate both buffers of train_state and train_batch.
-      donate_argnums=None,
+      donate_argnums=(),
   )
 
   train_metrics, train_summary = [], None
