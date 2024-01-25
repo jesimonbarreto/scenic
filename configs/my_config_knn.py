c6a829145959f9656c6089e1db52252b7d3b4da9
Jesimon Barreto <jesimonbarreto@github.com>
2024-01-25 14:59:56 -0300

    Updating

--- configs/my_config_knn.py
+++ configs/my_config_knn.py
@@ -1,18 +1,5 @@
 import ml_collections, os
 
-
-DATASET_TRAIN_SIZE = { #number of train images per dataset
-    'cars': 6346,
-    'sop': 48942,
-    'inshop': 20897,
-    'inat': 273929,
-    'met': 397121,
-    'gldv2': 1422914,
-    'food2k': 472349,
-    'rp2k': 188724,
-}
-
-
 ViT_configs = {
     'B/16': {
     "hidden_size" : 768,
@@ -26,15 +13,10 @@ ViT_configs = {
 
 
 
-def get_aggregated_size(datasets):
-
-  size = 0
-  
-  for dataset in datasets.split(','):
-    size += DATASET_TRAIN_SIZE[dataset]
-  
-  return size
-
+VARIANT = 'B/16'
+_IMAGENET_TRAIN_SIZE = 9469 #1281167
+MEAN_RGB = [0.485, 0.456, 0.406]
+STDDEV_RGB = [0.229, 0.224, 0.225]
 
 def get_config():
 
@@ -59,120 +41,62 @@ def get_config():
   config.eval_batch_size = 32 #batch size for extracting embeddings
   config.knn_eval_batch_size = 32 #batch size for batch knn search 
   
-  #merged eval
-  config.disabled_separate_knns = 'train_knn,val_knn,test_knn'
-  config.disabled_merged_knns = 'train_knn,val_knn'
-
-  #separate eval
-  # config.disabled_separate_knns = 'train_knn,val_knn'
-  # config.disabled_merged_knns = 'train_knn,val_knn,test_knn'
-
-  config.dataset_configs = ml_collections.ConfigDict()
-  
-  config.classifier = "joint" 
-  #config.classifier = "separate"
-
-  config.count_flops = False #bugged?
-
-  # Model.
-  config.model_class = 'vit_with_embedding'
-  
-  config.model_type = "B/16"
-
-  config.clip = False
-
-  config.model = ml_collections.ConfigDict()
-
-  #move these to the dictionary of the models
-  config.model.hidden_size = ViT_configs[config.model_type]["hidden_size"]
-  config.model.patches = ml_collections.ConfigDict()
-  config.model.patches.size = ViT_configs[config.model_type]["patches_size"]
-  config.model.num_heads = ViT_configs[config.model_type]["num_heads"]
-  config.model.mlp_dim = ViT_configs[config.model_type]["mlp_dim"]
-  config.model.num_layers = ViT_configs[config.model_type]["num_layers"]
-  config.model.representation_size = None #we will always use that as None
-  
-  config.model.output_dim = 64 #our chosen embedding dimension
-  
-  config.model.classifier = 'token'
-  config.model.attention_dropout_rate = 0.0
-  config.model.dropout_rate = 0.0
-  
-  config.model_dtype_str = 'float32'
-  #config.model_dtype_str = 'bfloat16'
-  
-  #config.model.positional_embedding = 'none'
-  config.model.positional_embedding = 'learned_1d'
-
-  config.transform_logits_type = 'normface'
-
-  #checkpoints
-  config.pretrained_ckpt_dir = ''
-  config.pretrained_ckpt = os.path.join(config.pretrained_ckpt_dir,ViT_configs[config.model_type]["checkpoint"])
-  
   # Training.
-  config.optimizer = 'adam'
-  config.optimizer_configs = ml_collections.ConfigDict()
-  config.optimizer_configs.beta1 = 0.9
-  config.optimizer_configs.beta2 = 0.999
-  config.optimizer_configs.weight_decay = 1e-6
-  config.explicit_weight_decay = None  # No explicit weight decay
-  config.l2_decay_factor = None
-  # config.max_grad_norm = 1.0
-  config.label_smoothing = None
+  config.max_grad_norm = 1
+  config.num_training_epochs = 400
+  config.batch_size = 512
+  config.steps_per_epoch = _IMAGENET_TRAIN_SIZE // config.batch_size
+  config.rng_seed = 42
+  total_steps = config.num_training_epochs * config.steps_per_epoch
+  config.global_crops_scale = (0.4, 1.0) 
+  config.local_crops_number = 0 #if 0, global scale = 0.14,1.0
+  config.local_crops_scale = (0.05,0.4)
+  config.student_temp = 0.1
+  config.center_momentum = 0.9
+  config.ncrops = 8
+  config.warmup_teacher_temp = 0.04
+  config.teacher_temp = 0.04
+  config.warmup_teacher_temp_epochs = 0
+  config.dataset_configs.number_of_focal_queries = n_queries - 1
 
-  config.num_training_epochs = 30
+  config.dataset_configs.pp_train = (
+      'decode' +
+      '|copy("image", "x1")' +
+      '|copy("image", "x2")' +
+      f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("image", "x1"), outkey=("image", "x1"))' +
+      f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("image", "x2"), outkey=("image", "x2"))' +
+      '|value_range(0, 1, data_key="x1")' +
+      '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x1")' +
+      '|random_grayscale(0.2, data_key="x1")' +
+      '|random_blur(1.0, data_key="x1")' +
+      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="x1")'
+
+      '|value_range(0, 1, data_key="x2")' +
+      '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x2")' +
+      '|random_grayscale(0.2, data_key="x2")' +
+      '|random_blur(0.1, data_key="x2")' +
+      '|random_solarize(0.2, data_key="x2")' +
+      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="x2")' +
+
+      ''.join([f'|copy("image", "crops{i}")' for i in range(config.ncrops)]) +
+      ''.join([f'|generate_crops((96, 96), {config.local_crops_scale}, inkey=("crops{i}"), outkey=("crops{i}"))' for i in range(config.ncrops)]) +
+      ''.join([f'|value_range(0, 1, data_key="crops{i}")' for i in range(config.ncrops)]) +
+      ''.join([f'|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="crops{i}")' for i in range(config.ncrops)]) +
+      ''.join([f'|random_grayscale(0.2, data_key="crops{i}")' for i in range(config.ncrops)]) +
+      ''.join([f'|random_blur(0.5, data_key="crops{i}")' for i in range(config.ncrops)]) +
+      ''.join([f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="crops{i}")' for i in range(config.ncrops)]))
+      #'|keep("x1, x2"' + ''.join([f', "crops{i}"' for i in range(config.ncrops)]) + ')')
+
   
-  config.batch_size = 128
-
-  config.steps_per_epoch = (get_aggregated_size(config.dataset_name) // config.batch_size)
-
-  config.rng_seed = 0 
-
-  config.init_head_bias = -10.0 
-  config.loss = ml_collections.ConfigDict()
-  config.loss.m = 0.0
-  config.loss.scale = 16
-  config.max_to_keep = 1000
-
-  config.log_eval_steps = config.steps_per_epoch
-
-  # Learning rate.
-  base_lr = 1e-3
-
-  config.lr_configs = ml_collections.ConfigDict()
-  config.lr_configs.learning_rate_schedule = 'compound'
-  config.lr_configs.factors = 'constant'
-  config.lr_configs.base_learning_rate = base_lr
-
-  config.lr_configs.backbone = ml_collections.ConfigDict()
-
-  #that means for 2 epochs we train only the classifier
-  config.lr_configs.backbone.frozen_steps = (
-      2 * config.steps_per_epoch
-  )
-
-  config.lr_configs.backbone.base_learning_rate = base_lr * 1e-2
+  # For IMAGENET-1K
+  #config.dataset_configs.dataset = 'imagenet2012'
+  config.dataset_configs.dataset = 'imagenette'
+  config.dataset_configs.train_split = 'train'
 
   ### kNN
 
   #dir of checkpoints
   config.train_dir = '/home/jesimonbarreto/scenic/dino_dir_test1/'
   
-
-  config.top_k = 5 #top k neighbors to look at
-  #config.top_k = 100
-
-  config.knn_start_epoch = 3
-  config.knn_end_epoch = 3 #set this to a lower value than start_epoch to not do knn at all
-
-  config.log_csv = False
-
-  config.save_neighbors = False
-
-  config.info_files_dir = ''
-
-  #config.descr_save_path = "."
-  config.descr_save_path = None
 
   return config
