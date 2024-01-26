6318f40e0ef8faeb0e8545327a5fff2274b2d8e2
Jesimon Barreto <jesimonbarreto@github.com>
2024-01-26 14:16:24 -0300

    Updating

--- configs/my_config_knn.py
+++ configs/my_config_knn.py
@@ -32,7 +32,7 @@ def get_config():
   config.dataset_configs.pp_train = (
       'decode' +
       '|copy("image", "image_resized")' +
-      '|copy("label", "label_im")' +
+      '|get_onehot(10)' +
       f'|copy_resize_file(224, inkey=("image", "image_resized"), outkey=("image", "image_resized"))' +
       '|value_range(0, 1, data_key="image_resized")' +
       '|value_range(0, 1, data_key="image")' +
