039b39e65367d9f30652fd481ce43b7dd87c4a00
Jesimon Barreto <jesimonbarreto@github.com>
2024-01-26 13:01:41 -0300

    Updating

--- configs/my_config_knn.py
+++ configs/my_config_knn.py
@@ -1,17 +1,5 @@
 import ml_collections, os
 
-ViT_configs = {
-    'B/16': {
-    "hidden_size" : 768,
-    "patches_size" : [16, 16],
-    "num_heads" : 12,
-    "mlp_dim" : 3072,
-    "num_layers" : 12,
-    "checkpoint" : 'B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res_224.npz'
-  },
-}
-
-
 
 VARIANT = 'B/16'
 _IMAGENET_TRAIN_SIZE = 50000 #9469 #1281167
@@ -44,8 +32,8 @@ def get_config():
   config.dataset_configs.pp_train = (
       'decode' +
       '|copy("image", "x1")' +
-      f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("image", "x1"), outkey=("image", "x1"))' +
-      f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("image", "x2"), outkey=("image", "x2"))' +
+      f'|copy_resize_file(224, inkey=("image", "x1"), outkey=("image", "x1"))' +
+      f'|copy_resize_file(224, inkey=("image", "x2"), outkey=("image", "x2"))' +
       '|value_range(0, 1, data_key="x1")' +
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="x1")'
   )
