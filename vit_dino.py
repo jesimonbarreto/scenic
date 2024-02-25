650372abd8382aa12fb804b4bcd4950bfb705174
Jesimon Barreto <jesimonbarreto@github.com>
2024-02-24 21:04:49 -0300

    Updating

--- vit_dino.py
+++ vit_dino.py
@@ -139,9 +139,9 @@ class ViTDINO(nn.Module):
 
     # If we want to add a class token, add it here.
     #if self.classifier == 'token':
-    cls = self.param('cls', nn.initializers.zeros, (1, 1, c), x.dtype)
-    
-    cls = jnp.asarray(cls)
+    cls = self.param('cls', nn.initializers.zeros, [1, 1, c], x.dtype)
+    if isinstance(cls, tuple):
+      cls = np.array(cls, dtype=x.dtype)
     print(f' data  {cls}')
     cls = jnp.tile(cls, [n, 1, 1])
     x = jnp.concatenate([cls, x], axis=1)
