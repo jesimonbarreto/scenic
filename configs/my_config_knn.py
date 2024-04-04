3090eb2c438bc601e54d093e3d2054500974a4f1
Jesimon Barreto <jesimonbarreto@github.com>
2024-04-04 16:06:20 -0300

    Updating

--- configs/my_config_knn.py
+++ configs/my_config_knn.py
@@ -25,6 +25,8 @@ def get_config():
   #for cifar 10
   config.dataset_configs.dataset = 'imagenet2012'
   config.dataset_configs.train_split = 'train'
+  config.dataset_configs.dataset_dir = 'gs://my_stg/storage/'
+
   config.dataset_configs.test_split = 'test'
   config.dataset_configs.batch_size_train = 1024
   config.dataset_configs.batch_size_test = 1024
