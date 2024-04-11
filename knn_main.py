bf3ec1a90b9a80b301944ee41279f39c4be3ae58
Jesimon Barreto <jesimonbarreto@github.com>
2024-04-11 19:08:40 -0300

    Updating

--- knn_main.py
+++ knn_main.py
@@ -268,7 +268,6 @@ def train(
       emb_train = emb_train.reshape((bl*bg, emb))
       label_train = label_train.reshape((bl*bg))
       jnp.savez(path_file, emb=emb_train, label=label_train)
-      break
 
 
     print('Finishing extract features train')
