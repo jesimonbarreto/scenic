e2e12a7bb59e8739d75bc18cef5635b70dedacdf
Jesimon Barreto <jesimonbarreto@github.com>
2024-04-25 14:54:27 -0300

    Updating

--- knn_main.py
+++ knn_main.py
@@ -261,7 +261,7 @@ def train(
       os.makedirs(dir_save_y)
 
     print('Starting to extract features train')
-    '''for i in range(config.steps_per_epoch):
+    for i in range(config.steps_per_epoch):
       path_file = os.path.join(dir_save_ckp,f'ckp_{step}_b{i}')
       #if os.path.isfile(path_file):
       #  continue
@@ -279,7 +279,7 @@ def train(
       emb_train = emb_train.reshape((bl*bg, emb))
       label_train = label_train.reshape((bl*bg))
       jnp.savez(path_file, emb=emb_train, label=label_train)
-    '''
+    
 
     print('Finishing extract features train')
     #print(dataset.meta_data.keys)
