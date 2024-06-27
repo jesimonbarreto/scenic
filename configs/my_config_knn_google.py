9b8132c9bea217d4a2e867383c1d844cbe5415cb
Jesimon Barreto <jesimonbarreto@github.com>
2024-06-27 17:10:50 -0300

    Updating

--- configs/my_config_knn_google.py
+++ configs/my_config_knn_google.py
@@ -52,7 +52,7 @@ def get_config():
       'decode' +
       '|copy("image", "image_resized")' +
       f'|adjust_labels({config.dataset_configs.desired_classes}, {config.num_classes},{config.dataset_configs.filter_classes}, key="label", key_result="label_adj")' +
-      f'|onehot({config.num_classes_filter}, key="label", key_result="label_onehot")' +
+      f'|onehot({config.num_classes_filter}, key="label_adj", key_result="label_onehot")' +
       '|resize_small(256, data_key="image")'+
       '|resize_small(256, data_key="image_resized")'+
       '|central_crop(224, data_key="image")'+
@@ -61,7 +61,7 @@ def get_config():
       '|value_range(0, 1, data_key="image_resized")' +
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image")'+
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image_resized")'+
-      '|keep("image", "image_resized", "label", "label_onehot")'
+      '|keep("image", "image_resized", "label_adj", "label", "label_onehot")'
   )
 
 
