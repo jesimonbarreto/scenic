8e5ce54071e005a3c5a1a5b5292dbe90992a4019
Jesimon Barreto <jesimonbarreto@github.com>
2024-07-31 15:42:19 -0300

    Updating

--- configs/dino_imnet1k_base16_last.py
+++ configs/dino_imnet1k_base16_last.py
@@ -32,7 +32,7 @@ def get_config():
   # Training.'MVImagenet'
   config.max_grad_norm = 1
   config.num_training_epochs = 2#400
-  config.batch_size = 1
+  config.batch_size = 8
   config.steps_per_epoch = _IMAGENET_TRAIN_SIZE // config.batch_size
   config.rng_seed = 42
   total_steps = config.num_training_epochs * config.steps_per_epoch
