b7d64281bb06f7c645a7ade4cb7374d515c37ffe
Jesimon Barreto <jesimonbarreto@github.com>
2024-08-13 23:27:31 -0300

    Updating

--- configs/dino_imnet1k_base16_last.py
+++ configs/dino_imnet1k_base16_last.py
@@ -126,6 +126,7 @@ def get_config():
                               'H': 1280}[version]
   config.model.patches = ml_collections.ConfigDict()
   config.model.patches.size = [patch, patch]
+  config.model.posembs = (518 // patch, 518 // patch)
   config.model.num_heads = {'Ti': 3, 'S': 6, 'B': 12, 'L': 16, 'H': 16}[version]
   config.model.mlp_dim = {'Ti': 768,
                           'S': 1536,
