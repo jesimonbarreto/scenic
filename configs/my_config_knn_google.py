ccf0207948f3c99922122a37356efec919f867f5
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-08-27 14:28:41 -0300

    Updating

--- configs/my_config_knn_google.py
+++ configs/my_config_knn_google.py
@@ -75,8 +75,7 @@ def get_config():
 
   #dir of checkpoints
   config.train_dir = '/home/jesimonbarreto/check_val/'#'/home/jesimonbarreto/exp_test_now/'
-  config.preextracted = False
-  #config.preextracted = True
+  config.preextracted = True
   config.write_summary = True
   config.steps_checkpoints = [3501]
   config.ks = [5,10,20]
