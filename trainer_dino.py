e4194e97ab34cc2ff349333a170702566f418a91
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-08-30 22:55:40 -0300

    Updating

--- trainer_dino.py
+++ trainer_dino.py
@@ -320,7 +320,7 @@ def train(
     tx = optax.inject_hyperparams(optax.adamw)(
         learning_rate=learning_rate_fn, weight_decay=config.weight_decay,
         mask=weight_decay_mask,)
-  opt_state = jax.jit(tx.init, backend='cpu')(params[0])
+  opt_state = jax.jit(tx.init, backend='cpu')(params)
 
   # Create chrono class to track and store training statistics and metadata.
   chrono = train_utils.Chrono()
