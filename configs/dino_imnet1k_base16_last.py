cf2b1b89d6090de244e65164dc38d058e157bead
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-09-09 12:48:42 -0300

    Updating

--- configs/dino_imnet1k_base16_last.py
+++ configs/dino_imnet1k_base16_last.py
@@ -15,10 +15,10 @@ def get_config():
   config = ml_collections.ConfigDict()
   #WANDB
   config.project = 'Report'
-  config.experiment_name = 'Dino_8k2Mhead_lr0001_layerwise'
+  config.experiment_name = 'Dino_8k2Mhead_lr00001_finetun'
   #config
-  config.transfer_learning = False
-  config.layer_wise = True
+  config.transfer_learning = True
+  config.layer_wise = False
   config.print_lr_infos = False
   # Dataset.
   config.dataset_name = 'dino_dataset'
