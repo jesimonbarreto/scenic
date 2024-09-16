3536ad202948a335a481ccd22e5633a563f19fb9
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-09-16 15:26:05 -0300

    Updating

--- configs/dino_imnet1k_base16_last.py
+++ configs/dino_imnet1k_base16_last.py
@@ -17,7 +17,7 @@ def get_config():
   config = ml_collections.ConfigDict()
   #WANDB
   config.project = 'Report'
-  config.experiment_name = 'Dino_8k2Mhead_LwF_embed_correct'
+  config.experiment_name = 'Dino_8k2Mhead_LwF_embed_correct_0.05'
   #config
   config.transfer_learning = False
   config.layer_wise = False
