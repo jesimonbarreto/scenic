13892504021e7a49e9eb8a6c0d0c03fd9e0eff72
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-09-16 14:13:57 -0300

    Updating

--- trainer_dino_LwF.py
+++ trainer_dino_LwF.py
@@ -206,7 +206,8 @@ def dino_train_step(
       total_loss += loss_dino/2
       total_loss /=2
     
-    loss_total = (0.5*loss_dino)+(loss_lwfv*0.5)
+    alfa_loss = 0.2
+    loss_total = (alfa_loss*loss_dino)+((1-alfa_loss)*loss_lwfv)
 
     return loss_total, (loss_dino, loss_lwfv, center)
   
