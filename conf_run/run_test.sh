d4cedab3f5b5c1e4938ce9c5607a77f09834efbb
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-09-23 17:16:53 -0300

    Updating

---
+++ conf_run/run_test.sh
@@ -0,0 +1,3 @@
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/dino_imnet1k_base16_last.py --workdir=../test
+sudo rm -rf ../test
--- configs/dino_imnet1k_base16_last.py
+++ configs/dino_imnet1k_base16_last.py
@@ -40,7 +40,7 @@ def get_config():
   # Training.'MVImagenet'
   config.alpha_loss = 0.1
   config.max_grad_norm = 1
-  config.num_training_epochs = 17#400
+  config.num_training_epochs = 2#400
   config.batch_size = 64
   config.steps_per_epoch = _IMAGENET_TRAIN_SIZE // config.batch_size
   config.rng_seed = 42
