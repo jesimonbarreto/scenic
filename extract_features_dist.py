a8be863f5d22d27c08e527c91aa03d5bbe205009
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-10-22 16:15:12 -0300

    Updating

--- extract_features_dist.py
+++ extract_features_dist.py
@@ -313,7 +313,7 @@ def train(
         result = extract_features(img)
         #result = jnp.squeeze(result)
         img = jnp.squeeze(result['x_norm_clstoken'][0, 0, 0])
-        embedding = normalize(embedding)
+        embedding = normalize(img)
         #print(img.shape)
         np.save(dir_video+resul_name+'.npy', jnp.array(embedding))
         print('Saving')
