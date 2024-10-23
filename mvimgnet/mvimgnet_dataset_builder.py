40e532016b9ec6ca995270ce3bb3e82e7d6ec89a
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-10-23 20:53:38 -0300

    Updating

--- mvimgnet/mvimgnet_dataset_builder.py
+++ mvimgnet/mvimgnet_dataset_builder.py
@@ -200,7 +200,7 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     # Load the embedding vectors (1, 384) for each frame
     vectors = []
     for frame in frames_video:
-        npz_path = frame.replace('.jpg', '.npz')
+        npz_path = frame.replace('.jpg', '.npy')
         vectors.append(self.load_npz(npz_path))
 
     n_frames = len(vectors)
