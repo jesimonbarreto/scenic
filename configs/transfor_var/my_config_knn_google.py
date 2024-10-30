fb4f78fb78079302c53d118ca68834923de8cadd
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-10-30 11:49:33 -0300

    Updating

--- configs/transfor_var/my_config_knn_google.py
+++ configs/transfor_var/my_config_knn_google.py
@@ -14,7 +14,7 @@ def get_config():
   config = ml_collections.ConfigDict()
   #WANDB
   config.project = 'Eval_transfor'
-  config.experiment_name = 'Eval_video_transform'
+  config.experiment_name = 'Eval_video_transform_xx'
   config.extract_train = True
   # Dataset.
   config.dataset_name = 'eval_dataset'
