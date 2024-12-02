bd358d2bbeafaf7163f24d2c1e1f9f412ba4b704
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-02 18:28:58 -0300

    Updating

--- configs/my_config_knn_google.py
+++ configs/my_config_knn_google.py
@@ -14,7 +14,7 @@ def get_config():
   config = ml_collections.ConfigDict()
   #WANDB
   config.project = 'Eval_report'
-  config.experiment_name = 'Eval_Dino_dino_LwF'
+  config.experiment_name = 'Eval_Dino_dino_frame_selection'
   config.extract_train = True
   # Dataset.
   config.dataset_name = 'eval_dataset'
@@ -76,7 +76,7 @@ def get_config():
   ### kNN
 
   #dir of checkpoints
-  config.train_dir = '/home/jesimonbarreto/test'#'/home/jesimonbarreto/exp_test_now/'
+  config.train_dir = '/home/jesimonbarreto/dino_run'#'/home/jesimonbarreto/exp_test_now/'
   config.preextracted = False
   config.write_summary = True
   config.steps_checkpoints = [5253]
