e98b0c8ad25f70c92f829f6a8bfdd0f3455342c2
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-10 16:51:32 -0300

    Updating

--- conf_run/run_test.sh
+++ conf_run/run_test.sh
@@ -1,5 +1,4 @@
 cd /home/jesimonbarreto/scenic
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/dino_imnet1k_base16_last_old.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
