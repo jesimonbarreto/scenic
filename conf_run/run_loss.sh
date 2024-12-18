e3454e4594f3d522c6ae24e8220c300eb90f8292
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 20:18:08 -0300

    Updating

---
+++ conf_run/run_loss.sh
@@ -0,0 +1,11 @@
+### test if you need sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
+cd /home/jesimonbarreto/scenic
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss1.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss2.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
