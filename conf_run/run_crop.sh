b2e348ed1cef4074646e24188b28a8b5c8fbf821
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 20:20:52 -0300

    Updating

--- conf_run/run_crop.sh
+++ conf_run/run_crop.sh
@@ -2,10 +2,10 @@
 cd /home/jesimonbarreto/scenic
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/crop_var/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop1.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/crop_var/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop2.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/crop_var/my_config_knn_google_old.py --workdir=../test_
--- conf_run/run_loss.sh
+++ conf_run/run_loss.sh
@@ -2,10 +2,10 @@
 cd /home/jesimonbarreto/scenic
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/loss_var/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss1.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/loss_var/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss2.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/loss_var/my_config_knn_google_old.py --workdir=../test_
