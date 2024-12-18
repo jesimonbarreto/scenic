4a26aa1642341a91a75a8d202ffe168b7d2db238
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 20:28:40 -0300

    Updating

--- configs/loss_var/my_config_knn_google_old.py
+++ configs/loss_var/my_config_knn_google_old.py
@@ -29,8 +29,8 @@ def get_config():
   config.dataset_configs.dataset_dir = '/mnt/disks/dataset/dataset/imagenet/'
   config.dataset_configs.train_split = 'train'
   config.dataset_configs.test_split = 'validation'
-  config.dataset_configs.batch_size_train = 256
-  config.dataset_configs.batch_size_test = 64
+  config.dataset_configs.batch_size_train = 512
+  config.dataset_configs.batch_size_test = 128
   config.num_classes = 1000
   reference_resolution = 224
   crop_size = 224
