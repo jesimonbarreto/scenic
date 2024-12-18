56c42a0deb4a09c4ecdd05b3b49f265289907401
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 20:10:31 -0300

    Updating

---
+++ conf_run/run_crop.sh
@@ -0,0 +1,11 @@
+### test if you need sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
+cd /home/jesimonbarreto/scenic
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop1.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop2.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
--- configs/my_config_knn_google_old.py
+++ configs/my_config_knn_google_old.py
@@ -13,7 +13,7 @@ def get_config():
   """Returns the ViT experiment configuration."""
   config = ml_collections.ConfigDict()
   #WANDB
-  config.project = 'Eval_new_Self'
+  config.project = 'Eval_new_crop'
   config.experiment_name = 'Eval_Dino_8k2Mhead_video'
   config.extract_train = True
   # Dataset.
