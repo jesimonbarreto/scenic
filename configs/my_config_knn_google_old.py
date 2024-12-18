2dc32a0835d7801da27fc41d67e7cc7e30e5bcc0
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 12:00:47 -0300

    Updating

--- configs/my_config_knn_google_old.py
+++ configs/my_config_knn_google_old.py
@@ -13,7 +13,7 @@ def get_config():
   """Returns the ViT experiment configuration."""
   config = ml_collections.ConfigDict()
   #WANDB
-  config.project = 'Eval_new'
+  config.project = 'Eval_new_sel'
   config.experiment_name = 'Eval_Dino_8k2Mhead_random_limited'
   config.extract_train = True
   # Dataset.
