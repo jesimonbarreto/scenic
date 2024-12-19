815196bf4a2bb5aa3ce1beacce9979f114ea70cc
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 23:15:21 -0300

    Updating

--- conf_run/run_crop.sh
+++ conf_run/run_crop.sh
@@ -9,3 +9,12 @@ sudo -E python -m knn_main --config=configs/crop_var/my_config_knn_google_old.py
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop2.py --workdir=../test
 sudo -E python -m knn_main --config=configs/crop_var/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss.py --workdir=../test
+sudo -E python -m knn_main --config=configs/loss_var/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss1.py --workdir=../test
+sudo -E python -m knn_main --config=configs/loss_var/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/loss_var/dino_mvimnet_lim_loss2.py --workdir=../test
+sudo -E python -m knn_main --config=configs/loss_var/my_config_knn_google_old.py --workdir=../test_
