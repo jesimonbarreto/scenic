d158b4b73ccb91cfbb2a3c02833b0a7ea09f9cc2
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-19 15:24:08 -0300

    Updating

--- mvimgnet/mvimgnet_dataset_builder_select_frames_pairs.py
+++ mvimgnet/mvimgnet_dataset_builder_select_frames_pairs.py
@@ -185,7 +185,7 @@ class Builder(tfds.core.GeneratorBasedBuilder):
   def check_npz_exists(self, frames_video):
       """Checks if all .npz files corresponding to the frames exist."""
       for frame in frames_video:
-          npz_path = frame.replace('.jpg', '.npz')
+          npz_path = frame.replace('.jpg', '.npy')
           if not os.path.exists(npz_path):
               return False
       return True
