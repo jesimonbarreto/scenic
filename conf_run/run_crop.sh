71a46fca6662570ab35a03d061d067fa2bf0d096
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-18 23:17:55 -0300

    Updating

--- conf_run/run_crop.sh
+++ conf_run/run_crop.sh
@@ -1,6 +1,9 @@
 ### test if you need sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
 cd /home/jesimonbarreto/scenic
 sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/batch_var/dino_mvimnet_lim_batch512.py --workdir=../test
+sudo -E python -m knn_main --config=configs/batch_var/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/crop_var/dino_mvimnet_lim_crop.py --workdir=../test
 sudo -E python -m knn_main --config=configs/crop_var/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
