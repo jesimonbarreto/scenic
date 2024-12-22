92e17edd7dd3ab47ba502c8f6d5d6eb489f007c4
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-22 13:46:48 -0300

    Updating

---
+++ conf_run/run_bas.sh
@@ -0,0 +1,13 @@
+### test if you need sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
+cd /home/jesimonbarreto/scenic
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselines.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselines.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselines.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselines.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselines.py --workdir=../test_
+
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselineb.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselineb.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselineb.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselineb.py --workdir=../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old_baselineb.py --workdir=../test_
