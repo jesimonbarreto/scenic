38fe9fbf18b3bad251f86cf20dd8baf4879d8751
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-23 14:14:03 -0300

    Updating

--- conf_run/run_test.sh
+++ conf_run/run_test.sh
@@ -4,14 +4,31 @@ sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_crop.py --workdir=../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_loss.py --workdir=../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_batch.py --workdir=../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
