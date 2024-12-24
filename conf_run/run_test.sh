9247975bc6e1ebedff91245452551380d3c3f3fc
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-24 15:43:42 -0300

    Updating

--- conf_run/run_test.sh
+++ conf_run/run_test.sh
@@ -13,7 +13,16 @@ sudo rm -rf ../test
 sudo rm -rf /mnt/disks/dataset/eval_files/
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo rm -rf /mnt/disks/dataset/eval_files/
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 
+
+sudo rm -rf ../test
+sudo rm -rf /mnt/disks/dataset/eval_files/
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 sudo rm -rf ../test
 sudo rm -rf /mnt/disks/dataset/eval_files/
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
