335a91205eb18f38f1af04080890f847ef0c7a65
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-24 08:26:09 -0300

    Updating

--- conf_run/run_test.sh
+++ conf_run/run_test.sh
@@ -9,26 +9,4 @@ sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdi
 sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test
