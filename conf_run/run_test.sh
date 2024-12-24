74ec0e82d6bb4914125ee81bcfe000fd177ccf70
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-24 08:40:26 -0300

    Updating

--- conf_run/run_test.sh
+++ conf_run/run_test.sh
@@ -10,3 +10,13 @@ sudo rm -rf ../test
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=../test
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=../test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test
