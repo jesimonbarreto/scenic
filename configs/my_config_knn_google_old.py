e7d56e17d2430cd313d1c4fa25dd970509b6b995
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-25 09:10:22 -0300

    Updating

--- configs/my_config_knn_google_old.py
+++ configs/my_config_knn_google_old.py
@@ -76,7 +76,7 @@ def get_config():
   ### kNN
 
   #dir of checkpoints
-  config.train_dir = '/home/jesimonbarreto/test'#'/home/jesimonbarreto/exp_test_now/'
+  config.train_dir = '/mnt/disks/dataset/test'#'/home/jesimonbarreto/exp_test_now/'
   config.preextracted = False
   config.write_summary = True
   config.steps_checkpoints = [8000]
