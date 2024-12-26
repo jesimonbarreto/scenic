adb00de51ccb3230cd2666e3976dabd5d6e8f88b
Jesimon Barreto <jesimonbarreto@gmail.com>
2024-12-26 20:04:06 -0300

    Updating

--- configs/exp_var/dino_mvimnet_lim_ep.py
+++ configs/exp_var/dino_mvimnet_lim_ep.py
@@ -208,7 +208,7 @@ def get_config():
   config.save_state_0 = False
   config.xprof = True  # Profile using xprof.
   config.checkpoint = True  # Do checkpointing.
-  config.checkpoint_steps = int(config.steps_per_epoch) * 5
+  config.checkpoint_steps = 5000
   config.log_summary_steps = 5
   config.max_keep_checkpoint = 20
 
