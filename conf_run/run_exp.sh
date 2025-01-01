ce09683773f514d379f3a7517380cb84ef7a8963
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-01-01 14:21:18 -0300

    Updating

---
+++ conf_run/run_exp.sh
@@ -0,0 +1,7 @@
+### test if you need sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
+cd /home/jesimonbarreto/scenic
+sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
+sudo rm -rf /mnt/disks/dataset/test
+sudo rm -rf /mnt/disks/dataset/eval_files/
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=/mnt/disks/dataset/test
+sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
