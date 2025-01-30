0abf1359f64435d23a2ef178f3d65f9cd59a39ee
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-01-30 14:57:57 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -1,12 +1,16 @@
 ### test if you need sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
 cd /home/jesimonbarreto/scenic
-sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
-sudo rm -rf /mnt/disks/dataset/test
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=/mnt/disks/dataset/test
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+#sudo rm -rf /mnt/disks/dataset/dataset/imagenet/mvimgnet/
+#sudo rm -rf /mnt/disks/dataset/test
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=/mnt/disks/dataset/test
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 # sudo rm -rf /mnt/disks/dataset/test
 # sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=/mnt/disks/dataset/test
 # sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
 # sudo rm -rf /mnt/disks/dataset/test
 # sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=/mnt/disks/dataset/test
 # sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
+sudo rm -rf ../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_dinos.py --workdir=../test_
+sudo rm -rf ../test_
+sudo -E python -m knn_main --config=configs/my_config_knn_google_dinob.py --workdir=../test_
