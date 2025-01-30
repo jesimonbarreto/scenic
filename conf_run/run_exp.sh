4636e10746c2482f74f899a05510afa783f914fc
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-01-30 15:01:02 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -10,7 +10,7 @@ cd /home/jesimonbarreto/scenic
 # sudo rm -rf /mnt/disks/dataset/test
 # sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim.py --workdir=/mnt/disks/dataset/test
 # sudo -E python -m knn_main --config=configs/my_config_knn_google_old.py --workdir=../test_
-sudo rm -rf ../test_
-sudo -E python -m knn_main --config=configs/my_config_knn_google_dinos.py --workdir=../test_
+#sudo rm -rf ../test_
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_dinos.py --workdir=../test_
 sudo rm -rf ../test_
 sudo -E python -m knn_main --config=configs/my_config_knn_google_dinob.py --workdir=../test_
