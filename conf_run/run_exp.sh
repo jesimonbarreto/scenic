d90d94d641c542515d469b511faee6489f2307ad
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-03 11:05:40 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -17,8 +17,8 @@ sudo -E python -m knn_main --config=configs/my_config_knn_google_old_video.py --
 #sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame.py --workdir=/mnt/disks/stg_dataset/test_frame
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_frame.py --workdir=../test_
 
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_scratch.py --workdir=/mnt/disks/stg_dataset/test_video_scratch
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_scratch.py --workdir=/mnt/disks/stg_dataset/test_video_scratch
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_video_scratch.py --workdir=../test_
 
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame_scratch.py --workdir=/mnt/disks/stg_dataset/test_frame_scratch
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame_scratch.py --workdir=/mnt/disks/stg_dataset/test_frame_scratch
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_frame_scratch.py --workdir=../test_
