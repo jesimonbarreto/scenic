974e6dc93d88b766e7fac28e74636d3a56ed370b
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-05 11:50:39 -0300

    Updating

--- configs/exp_var/dino_mvimnet_lim_frame_scratch.py
+++ configs/exp_var/dino_mvimnet_lim_frame_scratch.py
@@ -239,7 +239,7 @@ def get_config():
   config.checkpoint = True  # Do checkpointing.
   config.checkpoint_steps = 10000
   config.log_summary_steps = 5
-  config.max_keep_checkpoint = 4
+  config.max_keep_checkpoint = 2
 
   ####### Config val
 
