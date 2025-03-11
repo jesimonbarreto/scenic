a208d6e5051583b654e9df272989bebcb167c2f5
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-11 17:18:45 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -28,5 +28,5 @@ cd /home/jesimonbarreto/scenic
 #sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_frame_scratch.py --workdir=/mnt/disks/stg_dataset/test_test_
 #sudo -E python -m knn_main --config=configs/my_config_knn_google_old_frame_scratch.py --workdir=../test_
 
-
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/test_test
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn.py --workdir=/mnt/disks/stg_dataset/test_test2
