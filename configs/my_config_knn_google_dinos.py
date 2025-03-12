8fc2241f8aa50446f7d06b987e537790af571dd6
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-12 11:02:19 -0300

    Updating

--- configs/my_config_knn_google_dinos.py
+++ configs/my_config_knn_google_dinos.py
@@ -16,6 +16,7 @@ def get_config():
   config.project = 'Eval_Dino'
   config.experiment_name = 'Eval_Dino_v1_S'
   config.extract_train = True
+  config.transfer_learning = False
   # Dataset.
   config.dataset_name = 'eval_dataset'
   config.data_dtype_str = 'float32'
