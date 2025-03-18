7e69d00f7a652b6b0019b09337af17068cd66576
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-18 19:15:58 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -53,8 +53,8 @@ sudo rm -rf /mnt/disks/stg_dataset/test_test2
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_last_out.py --workdir=/mnt/disks/stg_dataset/test_test2
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
 sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_ex.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two.py --workdir=/mnt/disks/stg_dataset/test_test2
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
 sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_ex.py --workdir=/mnt/disks/stg_dataset/test_test2
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
