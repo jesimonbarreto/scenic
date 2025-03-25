37c73ca7c99256c6e028862492767654fb3ba1ca
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-25 10:33:34 -0300

    Updating

--- configs/exp_var/dino_mvimnet_lim_contlearn_two_h2.py
+++ configs/exp_var/dino_mvimnet_lim_contlearn_two_h2.py
@@ -176,7 +176,7 @@ def get_config():
   config.model.attention_dropout_rate = 0.0
   
   #head
-  config.model.n_layers = 4
+  config.model.n_layers = 3
   config.model.head_hidden_dim = 512
   config.model.head_bottleneck_dim = 256#64 #256
   
