9a61bb26c48f73b00f968a8cc3c6c1322270b628
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-25 14:55:12 -0300

    Updating

--- trainer_dino.py
+++ trainer_dino.py
@@ -170,21 +170,24 @@ def dino_train_step(
         train=True,
         rngs={'dropout': dropout_rng, 'droptok': droptok_rng})["x_train"]
     
-    cc = flax_model.apply(
-        {'params': params},
-        batch['sample'][1],
-        seqlen=config.reference_seqlen,
-        seqlen_selection=config.reference_seqlen_selection,
-        drop_moment=drop_moment,
-        backbone = True,
-        train=True,
-        rngs={'dropout': dropout_rng, 'droptok': droptok_rng})
+    if config.ncrops>0:
+      cc = flax_model.apply(
+          {'params': params},
+          batch['sample'][1],
+          seqlen=config.reference_seqlen,
+          seqlen_selection=config.reference_seqlen_selection,
+          drop_moment=drop_moment,
+          backbone = True,
+          train=True,
+          rngs={'dropout': dropout_rng, 'droptok': droptok_rng})
+      
+      student_out = jnp.concatenate([st,cc])
+    else:
+      student_out = st
     
-    student_out = jnp.concatenate([st,cc])
-    #student_out = st
     loss_dino, center = loss_fn(teacher_out, student_out, center, epoch)
     total_loss = loss_dino
-    
+
     return total_loss, (loss_dino, center)
   
   compute_gradient_fn = jax.value_and_grad(training_loss_fn, has_aux=True)
