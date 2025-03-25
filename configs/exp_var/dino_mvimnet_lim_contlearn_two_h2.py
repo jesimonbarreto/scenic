2aca7fa1706d9065f701d77681eca87bb332f3e8
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-25 15:28:22 -0300

    Updating

--- configs/exp_var/dino_mvimnet_lim_contlearn_two_h2.py
+++ configs/exp_var/dino_mvimnet_lim_contlearn_two_h2.py
@@ -72,7 +72,7 @@ def get_config():
   config.local_crops_scale = (0.05,0.25)
   config.student_temp = 0.3
   config.center_momentum = 0.9
-  config.ncrops = 4 #change other parameters
+  config.ncrops = 6 #change other parameters
   config.warmup_teacher_temp = 0.04
   config.teacher_temp = 0.05
   config.warmup_teacher_temp_epochs = 3
