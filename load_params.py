b536052508dab4a64790e0bf2dce28ba8310d91c
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-26 13:54:29 -0300

    Updating

--- load_params.py
+++ load_params.py
@@ -357,6 +357,7 @@ def restore_params(checkpoint_name: str, checkpoint_path: str, params: Any,
           k, v) in pt_weights.items() if k.startswith('encoder.')}
     def dontload(key):
       #adicionado Lora
+      print(key)
       if 'projecti' in key or 'UperNet' in key or 'fpn' in key or 'LoRa' in key or 'Lora' in key:
         return True
       if key.startswith('pixel_') or key.startswith('Dense_0'):
