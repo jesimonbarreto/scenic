4b1102b2ac6f6c4fe02d6cc93cc23583e8296bdd
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-03-29 08:35:25 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -4,46 +4,55 @@ cd /home/jesimonbarreto/scenic
 #sudo rm -rf /mnt/disks/stg_dataset/dataset/imagenet/mvimgnetest/
 
 ## Explora 
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_2.py --workdir=/mnt/disks/stg_dataset/test_test2
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
+#sudo rm -rf /mnt/disks/stg_dataset/test_test2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_2.py --workdir=/mnt/disks/stg_dataset/test_test2
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
 
 ## Explora training head
-sudo rm -rf /mnt/disks/stg_dataset/head_2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
+#sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2.py --workdir=/mnt/disks/stg_dataset/test_test2
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
+#sudo rm -rf /mnt/disks/stg_dataset/test_test2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2.py --workdir=/mnt/disks/stg_dataset/test_test2
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
 
 ## Explora training head with crops
-sudo rm -rf /mnt/disks/stg_dataset/head_2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
+#sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_crops.py --workdir=/mnt/disks/stg_dataset/test_test2
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
+#sudo rm -rf /mnt/disks/stg_dataset/test_test2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_crops.py --workdir=/mnt/disks/stg_dataset/test_test2
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
 
 ## Explora training head with loss
-sudo rm -rf /mnt/disks/stg_dataset/head_2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
+#sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_loss.py --workdir=/mnt/disks/stg_dataset/test_test2
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
+#sudo rm -rf /mnt/disks/stg_dataset/test_test2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_loss.py --workdir=/mnt/disks/stg_dataset/test_test2
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn.py --workdir=../test_
 
 ## Explora training head with last 2 training 
-sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head_2.py --workdir=/mnt/disks/stg_dataset/head_2
+
+#sudo rm -rf /mnt/disks/stg_dataset/test_test2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_two.py --workdir=/mnt/disks/stg_dataset/test_test2
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn_1.py --workdir=../test_
+
+## Explora training head 4 layers
+#sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
+
+#sudo rm -rf /mnt/disks/stg_dataset/test_test2
+#sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_head4.py --workdir=/mnt/disks/stg_dataset/test_test2
+#sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn_2.py --workdir=../test_
+
+
+## Explora training head with all best configurations
+sudo rm -rf /mnt/disks/stg_dataset/head_all
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head_2.py --workdir=/mnt/disks/stg_dataset/head_2
 
 sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_two.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_all.py --workdir=/mnt/disks/stg_dataset/test_test2
 sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn_1.py --workdir=../test_
-
-## Explora training head 4 layers
-sudo rm -rf /mnt/disks/stg_dataset/head_2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head.py --workdir=/mnt/disks/stg_dataset/head_2
-
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_head4.py --workdir=/mnt/disks/stg_dataset/test_test2
-sudo -E python -m knn_main --config=configs/my_config_knn_google_old_contiLearn_2.py --workdir=../test_
