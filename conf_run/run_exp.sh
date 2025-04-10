0bde7eb812d9f94807ad309329235a97a8cd7820
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-04-10 15:12:43 -0300

    Updating

--- conf_run/run_exp.sh
+++ conf_run/run_exp.sh
@@ -57,7 +57,7 @@ cd /home/jesimonbarreto/scenic
 
 ## Explora training head with our best configurations
 sudo rm -rf /mnt/disks/stg_dataset/head_2
-sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_head_all_b.py --workdir=/mnt/disks/stg_dataset/head_2
+sudo -E python -m main_dino --config=configs/exp_var/dino_co3d_lim_head_all_b.py --workdir=/mnt/disks/stg_dataset/head_2
 
 sudo rm -rf /mnt/disks/stg_dataset/test_test2
 sudo -E python -m main_dino --config=configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_all_proj_b.py --workdir=/mnt/disks/stg_dataset/test_test2
