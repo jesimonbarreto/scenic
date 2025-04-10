07161825438ec660331107013acf0a74af8f5a90
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-04-10 19:05:06 -0300

    Updating

--- configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_all_proj_b.py
+++ configs/exp_var/dino_mvimnet_lim_contlearn_two_h2_all_proj_b.py
@@ -58,7 +58,7 @@ def get_config():
   config.dataset_configs.shuffle_buffer_size = 250_000
   reference_resolution = 224
   n_queries = 10
-  config.mode = 'video' #'video_crops' # video or random
+  config.mode = 'frame' #'video_crops' # video or random
   
   #plot
   config.plot_ex = False
