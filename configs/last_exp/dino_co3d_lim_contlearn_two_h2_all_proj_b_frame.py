2e2e82bb4680f64a3f898a1778d7aadbcb9d89e9
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-04-16 15:07:45 -0300

    Updating

--- configs/last_exp/dino_co3d_lim_contlearn_two_h2_all_proj_b_frame.py
+++ configs/last_exp/dino_co3d_lim_contlearn_two_h2_all_proj_b_frame.py
@@ -159,6 +159,7 @@ def get_config():
         f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("x1", "x1"), outkey=("x1", "image1"))' +
         f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("x2", "x2"), outkey=("x2", "image1"))' +
         '|value_range(0, 1, data_key="x1")' +
+        '|cam_motion(max_translate=0.1, max_rotate=10.0, max_scale=0.05,brightness_delta=0.1, contrast_range=(0.9, 1.1), data_key="x1")' +
         '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x1")' +
         #'|random_color_jitter(0.8, 0.1, 0.1, 0.1, 0.1, data_key="x1")' +
         '|random_grayscale(0.2, data_key="x1")' +
