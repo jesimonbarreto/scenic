68d6c3017f73c0dd378c6ce12228681546a6ff79
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-04-30 16:19:38 -0300

    Updating

--- co3d/co3d_dataset_builder_percent.py
+++ co3d/co3d_dataset_builder_percent.py
@@ -65,11 +65,12 @@ class Builder(tfds.core.GeneratorBasedBuilder):
             #  encoding_format= 'jpeg'),
             'image1': tfds.features.Image(encoding_format='jpeg'),
             'image2': tfds.features.Image(encoding_format='jpeg'),
+            'label': tfds.features.ClassLabel(names=list(classes_co3d)),
         }),
         # If there's a common (input, target) tuple from the
         # features, specify them here. They'll be used if
         # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=('image1','image2'),  # Set to `None` to disable
+        supervised_keys=('image1','image2', 'label'),  # Set to `None` to disable
         homepage='https://dataset-homepage/',
     )
 
