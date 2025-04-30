f0f7a5ef0939d97e627aa728420963f823120e1c
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-04-30 10:32:58 -0300

    Updating

--- conf_run/run.sh
+++ conf_run/run.sh
@@ -11,11 +11,6 @@ sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_our.py --workdi
 
 sudo cp -r /mnt/disks/stg_dataset/test_test2 /mnt/disks/stg_dataset/mvimgnet_video
 
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_our_filter_clases_imgnet.py --workdir=/mnt/disks/stg_dataset/test_test2
-
-sudo cp -r /mnt/disks/stg_dataset/test_test2 /mnt/disks/stg_dataset/mvimgnet_video_filter
-
 
 sudo rm -rf /mnt/disks/stg_dataset/head_2
 sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_head_frame.py --workdir=/mnt/disks/stg_dataset/head_2
@@ -24,11 +19,6 @@ sudo rm -rf /mnt/disks/stg_dataset/test_test2
 sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_frame.py --workdir=/mnt/disks/stg_dataset/test_test2
 
 sudo cp -r /mnt/disks/stg_dataset/test_test2 /mnt/disks/stg_dataset/mvimgnet_frame
-
-sudo rm -rf /mnt/disks/stg_dataset/test_test2
-sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_frame_filter.py --workdir=/mnt/disks/stg_dataset/test_test2
-
-sudo cp -r /mnt/disks/stg_dataset/test_test2 /mnt/disks/stg_dataset/mvimgnet_frame_filter
 
 
 sudo -E python -m test_PCA_RGB --config=configs/final/mvimgnet/base_rgb.py --workdir=/mnt/disks/stg_dataset/test_
