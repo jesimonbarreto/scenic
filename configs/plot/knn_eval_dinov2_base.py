52d42989b6f891f8ad38574d659e5efaeaa5f8ea
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-05-10 16:50:03 -0300

    Updating

--- configs/plot/knn_eval_dinov2_base.py
+++ configs/plot/knn_eval_dinov2_base.py
@@ -69,7 +69,7 @@ def get_config():
       '|copy("image", "image_resized")' +
       #f'|adjust_labels({config.dataset_configs.desired_classes}, {config.num_classes},{config.dataset_configs.filter_classes}, key="label", key_result="label_adj")' +
       f'|onehot({config.num_classes_filter}, key="label", key_result="label_onehot")' +
-      '|adjust_ids(key="tfds_id", key_result="tfds_idi")' +
+      #'|adjust_ids(key="tfds_id", key_result="tfds_idi")' +
       '|resize_small(256, data_key="image")'+
       '|resize_small(256, data_key="image_resized")'+
       '|central_crop(224, data_key="image")'+
@@ -78,7 +78,7 @@ def get_config():
       '|value_range(0, 1, data_key="image_resized")' +
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image")'+
       f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image_resized")'+
-      '|keep("image", "image_resized", "tfds_idi")'
+      '|keep("image", "image_resized", "label", "label_onehot", "tfds_id")'
       #'|keep("image", "image_resized", "label_adj", "label", "label_onehot")'
   )
 
