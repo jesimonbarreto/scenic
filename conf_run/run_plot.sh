5c7810ccf227de43812695802385415d1662340d
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-05-10 19:08:46 -0300

    Updating

--- conf_run/run_plot.sh
+++ conf_run/run_plot.sh
@@ -1,7 +1,7 @@
 cd /home/jesimonbarreto/scenic
 
 
-sudo -E python -m knn_main_plot --config=configs/plot/knn_eval_dinov2_base.py --workdir=../test_
+#sudo -E python -m knn_main_plot --config=configs/plot/knn_eval_dinov2_base.py --workdir=../test_
 
 #sudo -E python -m main_dino --config=configs/plot/dinov2_nohead.py --workdir=/mnt/disks/stg_dataset/test_test2
 sudo -E python -m knn_main_plot --config=configs/plot/knn_eval_dinov2.py --workdir=../test_
