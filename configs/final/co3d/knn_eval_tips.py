d52b3bf1a591ef210590b637fb94d55342cf40c9
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-05-12 14:01:45 -0300

    Updating

--- configs/final/co3d/knn_eval_tips.py
+++ configs/final/co3d/knn_eval_tips.py
@@ -69,14 +69,12 @@ def get_config():
       '|copy("image", "image_resized")' +
       #f'|adjust_labels({config.dataset_configs.desired_classes}, {config.num_classes},{config.dataset_configs.filter_classes}, key="label", key_result="label_adj")' +
       f'|onehot({config.num_classes_filter}, key="label", key_result="label_onehot")' +
-      '|resize_small(256, data_key="image")'+
-      '|resize_small(256, data_key="image_resized")'+
-      '|central_crop(224, data_key="image")'+
-      '|central_crop(224, data_key="image_resized")'+
+      '|resize_small(448, data_key="image")'+
+      '|resize_small(448, data_key="image_resized")'+
+      '|central_crop(448, data_key="image")'+
+      '|central_crop(448, data_key="image_resized")'+
       '|value_range(0, 1, data_key="image")' +
       '|value_range(0, 1, data_key="image_resized")' +
-      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image")'+
-      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image_resized")'+
       '|keep("image", "image_resized", "label", "label_onehot")'
       #'|keep("image", "image_resized", "label_adj", "label", "label_onehot")'
   )
--- configs/final/co3d/tips_our.py
+++ configs/final/co3d/tips_our.py
@@ -377,8 +377,8 @@ def get_config():
       '|central_crop(224, data_key="image_resized")'+
       '|value_range(0, 1, data_key="image")' +
       '|value_range(0, 1, data_key="image_resized")' +
-      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image")'+
-      f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image_resized")'+
+      #f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image")'+
+      #f'|standardize({MEAN_RGB}, {STDDEV_RGB}, data_key="image_resized")'+
       '|keep("image", "image_resized", "label", "label_onehot")'
       #'|keep("image", "image_resized", "label_adj", "label", "label_onehot")'
   )
