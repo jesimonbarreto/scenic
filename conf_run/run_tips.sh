aebe063b24b15e56334eb51135003f48b6205b35
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-05-13 18:50:16 -0300

    Updating

--- conf_run/run_tips.sh
+++ conf_run/run_tips.sh
@@ -25,7 +25,7 @@ sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips.py --workdi
 
 
 #tips pretrained
-sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_tips_base.py --workdir=../test_
+#sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_tips_base.py --workdir=../test_
 
 #frame
 sudo rm -rf /mnt/disks/stg_dataset/head_2
