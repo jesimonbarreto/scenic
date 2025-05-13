48ce2a554c2d592b822b2115d9dd2f489d6ff05e
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-05-13 18:49:00 -0300

    Updating

--- configs/final/co3d/tips_our.py
+++ configs/final/co3d/tips_our.py
@@ -73,7 +73,7 @@ def get_config():
   config.teta_loss= 0.7
   config.max_grad_norm = 1
   config.num_training_epochs = 10#400
-  config.batch_size = 256
+  config.batch_size = 128
   config.steps_per_epoch = _IMAGENET_TRAIN_SIZE // config.batch_size
   config.rng_seed = 42
   total_steps = config.num_training_epochs * config.steps_per_epoch
@@ -124,8 +124,8 @@ def get_config():
         f'|copy("image2", "x2")'+
         ''.join([f'|copy("image{1 if i % 2 == 0 else 2}", "crop{i}")' for i in range(config.ncrops)]) +
 
-        f'|copy_resize_file(448, {config.global_crops_scale}, inkey=("x1", "x1"), outkey=("x1", "image1"))' +
-        f'|copy_resize_file(448, {config.global_crops_scale}, inkey=("x2", "x2"), outkey=("x2", "image2"))' +
+        f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("x1", "x1"), outkey=("x1", "image1"))' +
+        f'|copy_resize_file(224, {config.global_crops_scale}, inkey=("x2", "x2"), outkey=("x2", "image2"))' +
         '|value_range(0, 1, data_key="x1")' +
         '|random_color_jitter(0.8, 0.4, 0.4, 0.2, 0.1, data_key="x1")' +
         #'|random_color_jitter(0.8, 0.1, 0.1, 0.1, 0.1, data_key="x1")' +
