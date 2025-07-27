2cd7d953b41b137d44f303fe9616e45aedf89aca
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-07-27 18:36:16 -0300

    Updating

--- conf_run/run_ibot.sh
+++ conf_run/run_ibot.sh
@@ -25,12 +25,12 @@ cd /home/jesimonbarreto/scenic
 
 
 #tips pretrained
-sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_ibot_base.py --workdir=../test_
+#sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_ibot_base.py --workdir=../test_
 
 #video
-#sudo rm -rf /mnt/disks/stg_dataset/head_2
-#sudo -E python -m main_dino --config=configs/final/co3d/tips_head.py --workdir=/mnt/disks/stg_dataset/head_2
+sudo rm -rf /mnt/disks/stg_dataset/head_2
+sudo -E python -m main_dino --config=configs/final/co3d/ibot_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
-#sudo rm -rf /mnt/disks/stg_dataset/test_test2
-#sudo -E python -m main_dino --config=configs/final/co3d/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
-#sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips.py --workdir=../test_
+sudo rm -rf /mnt/disks/stg_dataset/test_test2
+sudo -E python -m main_dino --config=configs/final/co3d/ibot_our.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips.py --workdir=../test_
