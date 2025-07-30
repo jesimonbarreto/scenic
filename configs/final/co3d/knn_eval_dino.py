c6b7dcc434797cba3c8855cef1859ed24b539bf9
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-07-30 14:21:29 -0300

    Updating

--- configs/final/co3d/knn_eval_dino.py
+++ configs/final/co3d/knn_eval_dino.py
@@ -13,7 +13,7 @@ def get_config():
   """Returns the ViT experiment configuration."""
   config = ml_collections.ConfigDict()
   #WANDB
-  config.project = 'reb_exp'
+  config.project = 'Local_crops'
   config.experiment_name = 'eval_co3b'
   config.extract_train = True
   config.transfer_learning = False
--- configs/final/mvimgnet/knn_eval_dino.py
+++ configs/final/mvimgnet/knn_eval_dino.py
@@ -13,7 +13,7 @@ def get_config():
   """Returns the ViT experiment configuration."""
   config = ml_collections.ConfigDict()
   #WANDB
-  config.project = 'reb_exp'
+  config.project = 'LOCAL_CROPS'
   config.experiment_name = 'eval_mvimagenet'
   config.extract_train = True
   config.transfer_learning = False
