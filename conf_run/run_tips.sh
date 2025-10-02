6a7f105fa13fab8513f515edd9ad17b70e6c57c0
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-10-01 23:27:11 -0300

    Updating

--- conf_run/run_tips.sh
+++ conf_run/run_tips.sh
@@ -25,10 +25,10 @@ cd /home/jesimonbarreto/scenic
 
 
 #tips pretrained
-sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips_base.py --workdir=../test_
+#sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips_base.py --workdir=../test_
 
-sudo rm -rf /mnt/disks/stg_dataset/head_2
-sudo -E python -m main_dino --config=configs/final/co3d/tips_head.py --workdir=/mnt/disks/stg_dataset/head_2
+#sudo rm -rf /mnt/disks/stg_dataset/head_2
+#sudo -E python -m main_dino --config=configs/final/co3d/tips_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
 sudo rm -rf /mnt/disks/stg_dataset/test_test2
 sudo -E python -m main_dino --config=configs/final/co3d/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
