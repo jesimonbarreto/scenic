442b3d05c45750a1f59be6e56e7649ed34b7c241
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-10-12 17:56:58 -0300

    Updating

--- configs/final/co3d/tips_head.py
+++ configs/final/co3d/tips_head.py
@@ -60,7 +60,7 @@ def get_config():
   config.gama_loss = 1
   config.teta_loss= 0.7
   config.max_grad_norm = 1
-  config.num_training_epochs = 10#400
+  config.num_training_epochs = 5#400
   config.batch_size = 128
   config.steps_per_epoch = _IMAGENET_TRAIN_SIZE // config.batch_size
   config.rng_seed = 42
