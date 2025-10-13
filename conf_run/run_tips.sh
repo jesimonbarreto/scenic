034f2ae49da9684e68f10b27958691103d82c1a5
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-10-13 14:13:45 -0300

    Updating

--- conf_run/run_tips.sh
+++ conf_run/run_tips.sh
@@ -31,7 +31,7 @@ cd /home/jesimonbarreto/scenic
 #sudo -E python -m main_dino --config=configs/final/mvimgnet/tips_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
 sudo rm -rf /mnt/disks/stg_datsaset/test_test2
-sudo -E python -m main_dino --config=configs/final/mvimgnet/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m main_dino --config=configs/final/co3d/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
 sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_tips.py --workdir=../test_
 
 #frame
