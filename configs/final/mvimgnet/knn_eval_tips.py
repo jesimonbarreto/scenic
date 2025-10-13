fde99e7a85356f94af6aab5ef156124a8c6070fd
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-10-13 12:59:01 -0300

    Updating

--- configs/final/mvimgnet/knn_eval_tips.py
+++ configs/final/mvimgnet/knn_eval_tips.py
@@ -34,7 +34,7 @@ def get_config():
   # For IMAGENET-1K
   #config.dataset_configs.dataset = 'imagenet2012'
   #for cifar 10
-  config.dataset_configs.dataset = 'imagenet2012' #'mvimgnetest' #'mvimgnetest' #'co3dtest' #'imagenet2012'
+  config.dataset_configs.dataset = 'mvimgnetest' #'mvimgnetest' #'mvimgnetest' #'co3dtest' #'imagenet2012'
   config.dataset_configs.dataset_dir = '/mnt/disks/stg_dataset/dataset/imagenet/'
   config.dataset_configs.train_split = 'train'
   config.dataset_configs.test_split = 'validation'
