9158757469322ebb997f0ae3bca8b08fcb9696d3
Jesimon Barreto <jesimonbarreto@gmail.com>
2025-10-27 16:58:33 -0300

    Updating

--- conf_run/run_tips.sh
+++ conf_run/run_tips.sh
@@ -23,18 +23,38 @@ cd /home/jesimonbarreto/scenic
 #sudo -E python -m main_dino --config=configs/final/co3d/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
 #sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips.py --workdir=../test_
 
+sudo rm -rf /mnt/disks/stg_dataset/head_2
+sudo -E python -m main_dino --config=configs/final/mvimgnet/tips_head.py --workdir=/mnt/disks/stg_dataset/head_2
 
-#tips pretrained
-sudo -E python -m knn_main --config=configs/final/co3d/knn_eval_tips_base_b.py --workdir=../test_
-sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_tips_base_b.py --workdir=../test_
+sudo rm -rf /mnt/disks/stg_datsaset/test_test2
+sudo -E python -m main_dino --config=configs/final/mvimgnet/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_tips.py --workdir=../test_
 
 
-#sudo rm -rf /mnt/disks/stg_dataset/head_2
-#sudo -E python -m main_dino --config=configs/final/mvimgnet/tips_head.py --workdir=/mnt/disks/stg_dataset/head_2
+mkdir -p /mnt/disks/stg_datsaset/result_tips/
+sudo cp "$(ls /mnt/disks/stg_datsaset/test_test2/checkpoint_* | sort -t'_' -k2 -n | tail -n 1)" /mnt/disks/stg_datsaset/result_tips/
 
-#sudo rm -rf /mnt/disks/stg_datsaset/test_test2
-#sudo -E python -m main_dino --config=configs/final/co3d/tips_our.py --workdir=/mnt/disks/stg_dataset/test_test2
-#sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_tips.py --workdir=../test_
+
+sudo rm -rf /mnt/disks/stg_dataset/head_2
+sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_head.py --workdir=/mnt/disks/stg_dataset/head_2
+
+sudo rm -rf /mnt/disks/stg_datsaset/test_test2
+sudo -E python -m main_dino --config=configs/final/mvimgnet/dino_our.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_dino.py --workdir=../test_
+
+mkdir -p /mnt/disks/stg_datsaset/result_dino/
+sudo cp "$(ls /mnt/disks/stg_datsaset/test_test2/checkpoint_* | sort -t'_' -k2 -n | tail -n 1)" /mnt/disks/stg_datsaset/result_dino/
+
+sudo rm -rf /mnt/disks/stg_dataset/head_2
+sudo -E python -m main_dino --config=configs/final/mvimgnet/dinov2_head.py --workdir=/mnt/disks/stg_dataset/head_2
+
+sudo rm -rf /mnt/disks/stg_datsaset/test_test2
+sudo -E python -m main_dino --config=configs/final/mvimgnet/dinov2_our.py --workdir=/mnt/disks/stg_dataset/test_test2
+sudo -E python -m knn_main --config=configs/final/mvimgnet/knn_eval_dinov2.py --workdir=../test_
+
+mkdir -p /mnt/disks/stg_datsaset/result_dinov2/
+sudo cp "$(ls /mnt/disks/stg_datsaset/test_test2/checkpoint_* | sort -t'_' -k2 -n | tail -n 1)" /mnt/disks/stg_datsaset/result_dinov2/
+
 
 #frame
 #sudo rm -rf /mnt/disks/stg_dataset/head_2
